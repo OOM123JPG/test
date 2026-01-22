3df6a83c021d738b722e62c7cd9bb155678b2bc6
YANHONG WANG <ywang965@connect.hkust-gz.edu.cn>
2026-01-22 09:36:37 +0800

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,8 +1,10 @@
-export MASTER_ADDR=10.120.72.45
-export MASTER_PORT=29500
-export NODE_RANK=0
 export ASCEND_RT_VISIBLE_DEVICES=8,9,10,11,12,13,14,15
+export PYTORCH_NPU_ALLOC_CONF=max_split_size_mb:128
+export HCCL_BUFFSIZE=120
 
-torchrun --nproc_per_node=8 --nnodes=2 --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
-    inference_recap.py --model_path /home/GZGKD001/tmp/models/DeepSeek-V3-bf16 \
-    --whitening_dir /home/GZGKD001/tmp/yanhong/tdmoe_deepseek/output/decomp_results
+python /home/GZGKD001/tmp/yanhong/tdmoe_deepseek/distribution/reconstruct_inference.py \
+    --model_path /home/GZGKD001/tmp/models/DeepSeek-V3-bf16 \
+    --whitening_dir /home/GZGKD001/tmp/yanhong/tdmoe_deepseek/output/decompostion/wikitext2_n128 \
+    --node_rank 0 \
+    --master_addr 10.120.72.45 \
+    --layers 3 4 5 6 7 8 11 12 13 14 15 16 17 34 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 58 60
