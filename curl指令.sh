86d335bbf03d6fba989d33b2acae1d30cd7774f9
yuebin <27594071@qq.com>
2026-01-24 12:30:11 +0800

    添å新增curl指令

--- curl指令.sh
+++ curl指令.sh
@@ -1,33 +1,72 @@
-curl -X POST http://127.0.0.1:8888/v1/chat/completions \
-   -H "Content-Type: application/json" \
-   -d '{
-     "model": "deepseek-v3-tucker",
-     "messages": [{"role": "user", "content": "什么是人工智能？"}],
-     "max_tokens": 50,
-     "temperature": 0.7
-   }'
-
-
-curl http://127.0.0.1:8888/v1/completions \
+curl http://localhost:8888/v1/chat/completions \
   -H "Content-Type: application/json" \
   -d '{
     "model": "deepseek-v3-tucker",
-    "prompt": "The goal is to test loglikelihood.",
-    "temperature": 0,
-    "max_tokens": 1,
+    "messages": [
+      {"role": "user", "content": "你好，请问你是谁？"}
+    ],
+    "max_tokens": 32,
+    "temperature": 0.7
+  }'
+
+  curl http://localhost:8888/v1/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "deepseek-v3-tucker",
+    "prompt": "中国的首都是",
+    "max_tokens": 16,
+    "temperature": 0.0
+  }'
+
+  curl http://localhost:8888/v1/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "deepseek-v3-tucker",
+    "prompt": ["人工智能的未来是", "深度学习的核心是"],
+    "max_tokens": 10
+  }'
+
+curl http://localhost:8888/v1/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "deepseek-v3-tucker",
+    "prompt": "The capital of France is Paris",
     "logprobs": 1,
-    "echo": true
-  }'	
+    "echo": true,
+    "temperature": 0.0
+  }'
+
+  
+# curl -X POST http://127.0.0.1:8888/v1/chat/completions \
+#    -H "Content-Type: application/json" \
+#    -d '{
+#      "model": "deepseek-v3-tucker",
+#      "messages": [{"role": "user", "content": "什么是人工智能？"}],
+#      "max_tokens": 50,
+#      "temperature": 0.7
+#    }'
+
+
+# curl http://127.0.0.1:8888/v1/completions \
+#   -H "Content-Type: application/json" \
+#   -d '{
+#     "model": "deepseek-v3-tucker",
+#     "prompt": "The goal is to test loglikelihood.",
+#     "temperature": 0,
+#     "max_tokens": 1,
+#     "logprobs": 1,
+#     "echo": true
+#   }'	
 	
 	
 	   
-curl http://127.0.0.1:8888/v1/completions \
-  -H "Content-Type: application/json" \
-  -d '{
-    "model": "deepseek-v3-tucker",
-    "prompt": ["Option A is better.", "Option B is better."],
-    "temperature": 0,
-    "max_tokens": 1,
-    "logprobs": 1,
-    "echo": true
-  }'
+# curl http://127.0.0.1:8888/v1/completions \
+#   -H "Content-Type: application/json" \
+#   -d '{
+#     "model": "deepseek-v3-tucker",
+#     "prompt": ["Option A is better.", "Option B is better."],
+#     "temperature": 0,
+#     "max_tokens": 1,
+#     "logprobs": 1,
+#     "echo": true
+#   }'
